e862fd63e36b1b6a53411280e3adefbe30b3ec1b
jprorokova <54396978+jprorokova@users.noreply.github.com>
2020-03-05 21:02:42 +0200

    Update foo.h

--- Neew_______2/Neew_______2/foo.h
+++ Neew_______2/Neew_______2/foo.h
@@ -7,13 +7,14 @@
 #include <io.h>
 #include <windows.h>
 #include <TlHelp32.h>
+#include <vector>
 using namespace std;
 
 
 int Dir(int sum, int games, int competitors, string* teams, int*** arr, int* results);
 int team_numbers();
 void create_dynamic(int** arr, int games, int competitors);
-void data_processing(string* team, int*** arr, int games, int result, ifstream& file, int from, int to);
+void data_processing(int sum, string* team, int*** arr, int games, int result, ifstream& file, int from, int to);
 void delete_array(string* teams);
 void delete_array(int* result);
 void delete_dynamic(int*** arr, int games, int teams);
